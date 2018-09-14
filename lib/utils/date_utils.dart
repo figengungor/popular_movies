9b648a94ad25729fcfbd0d6e35d4c4edac717a20
figengungor <figengungor1@gmail.com>
2018-09-14 10:44:42 +0300

    Use parseStrict with date formatter for true conversion.

--- lib/utils/date_utils.dart
+++ lib/utils/date_utils.dart
@@ -4,7 +4,7 @@ class DateUtils {
   static String getYear(String dateStr) {
     try {
       var formatter = new DateFormat('yyyy-MM-dd');
-      DateTime date = formatter.parse(dateStr);
+      DateTime date = formatter.parseStrict(dateStr);
       return date.year.toString();
     } catch (error) {
       return dateStr;
